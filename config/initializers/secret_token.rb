caa9f8b492551eb4e548b0ec6d4897ad1452f81e
andy rogers <andy.rogers@teebox.co>
2012-07-29 18:42:59 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Canvas::Application.config.secret_token = '7c6fc0840b853e30bad151231c453cee3af7c651b992368307f3413e6298f6b2e7296dd3127f82444e33e470d446a0ea5c46ceb77e694f75b90ae15e50b22a97'
